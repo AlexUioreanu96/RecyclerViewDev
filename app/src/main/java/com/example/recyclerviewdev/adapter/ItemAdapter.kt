b77a9743a4ee8d63e9224d7c84d35b29dfcb703e
alexuioreanu <alexandru.uioreanu@yopeso.com>
2022-07-29 14:42:09 +0300

    Constructing onCreateViewHolder slowly

--- app/src/main/java/com/example/recyclerviewdev/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/recyclerviewdev/adapter/ItemAdapter.kt
@@ -1,12 +1,14 @@
 package com.example.recyclerviewdev.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerviewdev.R
 import com.example.recyclerviewdev.model.Affirmation
+
 
 class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) :
     RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
@@ -16,7 +18,10 @@ class ItemAdapter(private val context: Context, private val dataset: List<Affirm
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        TODO("Not yet implemented")
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+        return ItemViewHolder(adapterLayout)
+
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
